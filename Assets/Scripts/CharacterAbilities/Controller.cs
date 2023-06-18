d21bb77e16d2e30e81fd6e664d738e1aff836a12
GDIVX <yarin107@gmail.com>
2023-06-18 15:13:24 +0300

    .

--- Assets/Scripts/CharacterAbilities/Controller.cs
+++ Assets/Scripts/CharacterAbilities/Controller.cs
@@ -7,6 +7,10 @@ namespace CharacterAbilities.Assets.Scripts.CharacterAbilities
     // This is a base class for all controllers that can be used to control a character
     public abstract class Controller : MonoBehaviour
     {
+        /// <summary>
+        /// Returns a vector that represents the movement input
+        /// </summary>
+        /// <returns>Normolized Vector2</returns>
         public abstract Vector2 GetMovementVector();
     }
 }
